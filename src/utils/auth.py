fd726ba4dbc0b4f0958c309bc0c546a321545a52
Bentley Hensel <bentley.hensel@civicactions.com>
2023-05-09 16:31:48 +0000

    Improved logging & error handling

--- src/utils/auth.py
+++ src/utils/auth.py
@@ -2,58 +2,62 @@ import pika
 from utils.watch import logger
 
 def rabbit(queue_name, message):
-    logger.debug('Connecting to RabbitMQ server...')
-    credentials = pika.PlainCredentials('rabbit_hole', 'lets_insert_things')
-    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbit', credentials=credentials, virtual_host='gova11y'))
-    logger.debug('Connected to RabbitMQ server!')
-
-    logger.debug(f'Declaring queue: {queue_name}...')
-    channel = connection.channel()
-    channel.queue_declare(queue=queue_name, durable=True, arguments={'x-message-ttl': 7200000})
-    logger.debug(f'Queue {queue_name} declared!')
-
     try:
+        logger.debug('Connecting to RabbitMQ server...')
+        credentials = pika.PlainCredentials('rabbit_hole', 'lets_insert_things')
+        connection = pika.BlockingConnection(pika.ConnectionParameters('rabbit', credentials=credentials, virtual_host='gova11y'))
+        logger.debug('Connected to RabbitMQ server!')
+
+        logger.debug(f'Declaring queue: {queue_name}...')
+        channel = connection.channel()
+        channel.queue_declare(queue=queue_name, durable=True, arguments={'x-message-ttl': 7200000})
+        logger.debug(f'Queue {queue_name} declared!')
+
         channel.basic_publish(
             exchange='',
             routing_key=queue_name,
             body=message,
             properties=pika.BasicProperties(delivery_mode=2)  # Make the messages persistent
         )
-        logger.debug(f'Message published to {queue_name}')
-        channel.close()
-        connection.close()
-        logger.debug('Channel and connection closed')
-        return channel, connection
+        logger.info(f'Message sent to queue {queue_name}: {message}')
     except Exception as e:
-        logger.error(f"You've got a sick rabbit... {e}")
-        return None, None
+        logger.error(f"Error sending message to RabbitMQ: {e}")
+    finally:
+        try:
+            channel.close()
+            connection.close()
+            logger.debug('RabbitMQ connection closed')
+        except Exception as e:
+            logger.error(f"Error closing RabbitMQ connection: {e}")
 
 # Catching Rabbits
 def catch_rabbits(queue_name, process_func):
-    logger.debug('Connecting to RabbitMQ server...')
-    credentials = pika.PlainCredentials('rabbit_hole', 'lets_insert_things')
-    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbit', credentials=credentials, virtual_host='gova11y'))
-    logger.debug('Connected to RabbitMQ server!')
-
-    logger.debug(f'Declaring queue: {queue_name}...')
-    channel = connection.channel()
-    channel.queue_declare(queue=queue_name, durable=True, arguments={'x-message-ttl': 7200000})
-    logger.debug(f'Queue {queue_name} declared!')
-
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(
-        queue=queue_name,
-        on_message_callback=process_func,
-        auto_ack=False
-    )
-    logger.info(f'🐇 [*] Waiting for messages in {queue_name}. To exit press CTRL+C')
-
     try:
+        logger.debug('Connecting to RabbitMQ server...')
+        credentials = pika.PlainCredentials('rabbit_hole', 'lets_insert_things')
+        connection = pika.BlockingConnection(pika.ConnectionParameters('rabbit', credentials=credentials, virtual_host='gova11y'))
+        logger.debug('Connected to RabbitMQ server!')
+
+        logger.debug(f'Declaring queue: {queue_name}...')
+        channel = connection.channel()
+        channel.queue_declare(queue=queue_name, durable=True, arguments={'x-message-ttl': 7200000})
+        logger.debug(f'Queue {queue_name} declared!')
+
+        channel.basic_qos(prefetch_count=1)
+        channel.basic_consume(
+            queue=queue_name,
+            on_message_callback=process_func,
+            auto_ack=False
+        )
+        logger.info(f'🐇 [*] Waiting for messages in {queue_name}. To exit press CTRL+C')
+
         channel.start_consuming()
-    except KeyboardInterrupt:
-        logger.warn('CTRL+C pressed. Stopping message consumption...')
+    except Exception as e:
+        logger.error(f"Error in catch_rabbits function: {e}")
     finally:
-        channel.stop_consuming()
-        channel.close()
-        connection.close()
-        logger.debug('Channel and connection closed')
+        try:
+            channel.stop_consuming()
+            connection.close()
+            logger.debug('RabbitMQ connection closed')
+        except Exception as e:
+            logger.error(f"Error stopping consumer and closing RabbitMQ connection: {e}")
